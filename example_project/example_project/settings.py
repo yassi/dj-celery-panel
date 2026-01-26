43d880bd6aca25d41833aacab50a6c709d3b769b
Yasser Toruno <yasser@yassers-mbp.lan>
2026-01-26 13:08:46 -0500

    switch to using staticfiles for css

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -251,6 +251,7 @@ DJ_CELERY_PANEL_SETTINGS = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
